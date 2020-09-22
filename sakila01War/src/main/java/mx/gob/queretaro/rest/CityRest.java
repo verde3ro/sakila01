3cb5fa799926207356f0f13d93b28d54bfb4f65a
Raúl Alejandro Verde Martínez <rverde@queretaro.gob.mx>
2020-09-21 21:04:15 -0500

    Cambios finales

--- sakila01War/src/main/java/mx/gob/queretaro/rest/CityRest.java
+++ sakila01War/src/main/java/mx/gob/queretaro/rest/CityRest.java
@@ -6,6 +6,7 @@ import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -24,6 +25,13 @@ public class CityRest {
 	@Produces(MediaType.APPLICATION_JSON)
 	public List<City> obtenerTodos() {
 		return cityFacadeLocal.obtenerTodos();
+	}
+
+	@GET
+	@Path("obtenerPorId/{cityId}") // api/ciudad/obtenerPorId
+	@Produces(MediaType.APPLICATION_JSON)
+	public City obtenerPorId(@PathParam("cityId") short cityId) {
+		return cityFacadeLocal.obtenerPorIdJpql(cityId);
 	}
 
 }
